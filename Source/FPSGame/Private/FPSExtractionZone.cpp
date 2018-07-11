ab30e1a2f19b632166a8650e942a1c155393160c
Gerald <gerald.lang@mail.mcgill.ca>
2018-07-11 17:49:41 -0400

    Extraction Zone Added

---
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSExtractionZone.h"
+#include "Components/BoxComponent.h"
+#include "UnrealEngine.h"
+
+
+// Sets default values
+AFPSExtractionZone::AFPSExtractionZone()
+{
+	OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
+	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	OverlapComp->SetBoxExtent(FVector(200.0));
+	RootComponent = OverlapComp;
+	
+	OverlapComp->SetHiddenInGame(false);
+
+	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
+}
+
+void AFPSExtractionZone::HandleOverlap(class UPrimitiveComponent *OverlappedComponent, AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Green, TEXT("CUCKED"));
+	UE_LOG(LogTemp, Log, TEXT("Something entered the extraction zone..."));
+}
